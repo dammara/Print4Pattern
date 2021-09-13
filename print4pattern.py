0dcb31e5f4b1fd4af97558cd177681f1cf04e447
dammara <56162593+dammara@users.noreply.github.com>
2021-09-13 09:46:45 -0400

    Add files via upload

---
+++ print4pattern.py
@@ -0,0 +1,27 @@
+# Markhus Dammar
+# 13 September 2021
+# Bonus for 30 pts, instance 2
+
+print("""
+     +
+  +     +
++         +
+  +  +  +
+   +   +
+     *
+  *     *
+*         *
+  *  *  *
+   *   *
+     -
+  -     -
+-         -
+  -  -  -
+   -   -
+     $
+  $     $
+$         $
+  $  $  $
+   $   $
+      
+""")
